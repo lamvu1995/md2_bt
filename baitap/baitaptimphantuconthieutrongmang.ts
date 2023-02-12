9fc27a857a945371886190aa388b63c1001e1446
unknown <dange19954321@gmail.com>
2023-02-12 14:11:15 +0700

    bt

--- baitap/baitaptimphantuconthieutrongmang.ts
+++ baitap/baitaptimphantuconthieutrongmang.ts
@@ -1,7 +1,7 @@
 let numbers: number[] = [0, 2, 3, 5, 7, 8, 9, 10]
 let newarr: number[] = []
 for (let i = 0; i < numbers.length; i++) {
-    if(i !== numbers[i]) {
+    if(numbers.indexOf(i) == -1) {
         newarr.push(i)
     }
 }
